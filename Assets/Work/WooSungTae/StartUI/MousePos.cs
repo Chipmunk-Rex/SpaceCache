7fc7decc3da7d223ce8b96763ec8c6fb98ffccec
woosungtae605 <a01076755795@gmail.com>
2025-07-08 20:10:57 +0900

    feat : Start UI

---
+++ Assets/Work/WooSungTae/StartUI/MousePos.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class MousePos : MonoBehaviour
+{
+    [SerializeField] Rect range;
+    private bool iInRange = false;
+    private void Update()
+    {
+        Vector2 mouseScreenPos = Input.mousePosition;
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        if (range.Contains(mouseWorldPos))
+        {
+            iInRange = true;
+            Debug.Log("µé¾î¿È");
+        }
+        else
+        {
+            iInRange = false;
+        }
+    }
+
+    public bool OnRangeEnter()
+    {
+        return iInRange;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(range.center, new Vector2(range.width, range.height));
+    }
+}
