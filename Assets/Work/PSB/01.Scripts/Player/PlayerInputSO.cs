c0b5ebd975378a1fa0e6f749de1f811ddaaac120
PSB08 <Parkseongbin08@gmail.com>
2025-07-08 17:22:38 +0900

    remove : visual scripting, feat : dotween

--- Assets/Work/PSB/01.Scripts/Player/PlayerInputSO.cs
+++ Assets/Work/PSB/01.Scripts/Player/PlayerInputSO.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Work.PSB._01.Scripts.Player
@@ -6,7 +7,7 @@ namespace Work.PSB._01.Scripts.Player
     [CreateAssetMenu(fileName = "PlayerInputSO", menuName = "SO/Player", order = 0)]
     public class PlayerInputSO : ScriptableObject, Controls.IPlayerActions
     {
-        
+        public event Action OnAttackPressed;
         
         public void OnMove(InputAction.CallbackContext context)
         {
