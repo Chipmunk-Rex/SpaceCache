c0eadf20eea352e037510c363fd12f48343fba76
PSB08 <Parkseongbin08@gmail.com>
2025-07-08 17:36:58 +0900

    feat : Entity, StatSystem

--- Assets/Work/PSB/01.Scripts/Player/PlayerInputSO.cs
+++ Assets/Work/PSB/01.Scripts/Player/PlayerInputSO.cs
@@ -8,15 +8,36 @@ namespace Work.PSB._01.Scripts.Player
     public class PlayerInputSO : ScriptableObject, Controls.IPlayerActions
     {
         public event Action OnAttackPressed;
+
+        public Vector2 MovementKey { get; private set; }
+        private Controls _controls;
         
+        public bool IsCanAttack { get; set; }
+
+        private void OnEnable()
+        {
+            if (_controls == null)
+            {
+                _controls = new Controls();
+                _controls.Player.SetCallbacks(this);
+            }
+            _controls.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Player.Disable();
+        }
+
         public void OnMove(InputAction.CallbackContext context)
         {
-            
+            MovementKey = context.ReadValue<Vector2>();
         }
 
         public void OnAttack(InputAction.CallbackContext context)
         {
-            
+            if (IsCanAttack && context.performed)
+                OnAttackPressed?.Invoke();
         }
         
     }
