ee14095e131c9aadf0e4f1ceb90e47f6a84433ad
hanchunbae <gsiwon67@gmail.com>
2025-07-14 19:54:36 +0900

    EnemySpowner System Add

--- Assets/Script/EnemySpown.cs
+++ Assets/Script/EnemySpown.cs
@@ -2,19 +2,47 @@ using UnityEngine;
 
 public class EnemySPown : MonoBehaviour
 {
-    private int enemypoolsize = 50;
+    private int enemyPoolSize = 50;
     public GameObject enemyPrefab;
     private GameObject[] enemyPool;
 
+    public float spawnInterval = 1.0f; // 1초마다 적 스폰
+    private float timer = 0f;
+
     void Start()
     {
-        enemyPool = new GameObject[enemypoolsize];
-        for (int i = 0; i < enemypoolsize; i++)
+        enemyPool = new GameObject[enemyPoolSize];
+        for (int i = 0; i < enemyPoolSize; i++)
         {
             enemyPool[i] = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             enemyPool[i].SetActive(false);
         }
-        enemyPool[0].SetActive(true);
+
+        Debug.Log("적 풀 생성 완료");
     }
-    
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnEnemy();
+            timer = 0f;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        for (int i = 0; i < enemyPool.Length; i++)
+        {
+            if (!enemyPool[i].activeInHierarchy)
+            {
+                enemyPool[i].transform.position = transform.position; // 스폰 위치 초기화
+                enemyPool[i].SetActive(true);
+                Debug.Log("적 스폰됨: " + i);
+                break;
+            }
+        }
+    }
 }
