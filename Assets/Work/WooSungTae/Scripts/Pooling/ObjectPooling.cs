14a433b6e4c30d1e069b5e2a4f4b50e721442610
woosungtae <a01076755795@gmail.com>
2025-08-21 20:00:54 +0900

    ss

--- Assets/Work/WooSungTae/Scripts/Pooling/ObjectPooling.cs
+++ Assets/Work/WooSungTae/Scripts/Pooling/ObjectPooling.cs
@@ -3,14 +3,52 @@ using UnityEngine;
 
 public class ObjectPooling : MonoBehaviour
 {
-    private Dictionary<string,GameObject> enemyDic;
-    private List<EnemySo> enemyList = new();
+    [SerializeField] private int enemySpawnMaxCount; // ¿¡³Ê¹Ì ÃÑ ¼ÒÈ¯ ÇÑµµ È½¼ö
+
+    private Dictionary<EnemySo,Stack<GameObject>> enemyDic = new();
+    [SerializeField] private List<EnemySo> enemyList = new();
 
     private void Start()
     {
-        foreach(var enemy in enemyList)
+        EnemyInitialize();
+    }
+
+    private void EnemyInitialize()
+    {
+        foreach (var enemy in enemyList)
         {
+            Stack<GameObject> saveEnemy = new();
+            for(int i = 0; i < enemySpawnMaxCount; i++)
+            {
+                GameObject a = Instantiate(enemy.prefab, transform);
+                a.SetActive(false);
+                saveEnemy.Push(a);
+            }
+            enemyDic.Add(enemy, saveEnemy);
         }
     }
 
+    public GameObject SpawnEnemy(EnemySo so, Transform spawnPosition)
+    {
+        if (!enemyDic.ContainsKey(so)) return null;
+        GameObject a;
+        if (enemyDic[so].Count > 0)
+        {
+            a = enemyDic[so].Pop();
+        }
+        else
+        {
+            a = Instantiate(so.prefab, transform);
+        }
+        a.SetActive(true);
+        a.transform.position = spawnPosition.position;
+
+        return a;
+    }
+
+    public void ReturnEnemy(EnemySo so, GameObject enemy)
+    {
+         enemy.SetActive(false);
+         enemyDic[so].Push(enemy);
+    }
 }
