c147c63ae4678e88ecf7d35271f40637ce7319f4
bolsterstone <jerukim419@gmail.com>
2025-09-04 19:24:11 +0900

    Unsuck Update

--- Assets/Work/Yeru/01.Scripts/Enemy/UnSuck/UnSuckExplosion.cs
+++ Assets/Work/Yeru/01.Scripts/Enemy/UnSuck/UnSuckExplosion.cs
@@ -6,12 +6,22 @@ public class UnSuckExplosion : MonoBehaviour
     [SerializeField] private float damege = 100f; 
     [SerializeField] private string playerTag = "Player";
 
+    private float bonusDamage = 0f;
+    
+    
     private bool _fired; 
 
+    public void IncreaseAttack(float amount)
+    {
+        bonusDamage += amount;
+    }
+    
     public void Bob()
     {
         if (_fired) return;
         _fired = true;
+        
+        float finalDamage = damege + bonusDamage;
 
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, Range);
         foreach (Collider2D hit in hits)
@@ -21,7 +31,7 @@ public class UnSuckExplosion : MonoBehaviour
             
             if (hit.TryGetComponent(out IDamageable dmg))
             {
-                dmg.TakeDamage(damege);
+                dmg.TakeDamage(finalDamage);
             }
             else
             {
@@ -29,6 +39,7 @@ public class UnSuckExplosion : MonoBehaviour
             }
         }
     }
+    private void OnEnable() => _fired = false;
 
     private void OnDrawGizmosSelected()
     {
