1f3ba6c4b7bafd5184ec680ef3d4159da3273bf6
PSB08 <Parkseongbin08@gmail.com>
2025-09-12 17:18:01 +0900

    feat : Mana drop system

--- Assets/Work/PSB/01.Scripts/Core/ManaDropMap.cs
+++ Assets/Work/PSB/01.Scripts/Core/ManaDropMap.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using UnityEngine;
+using PSB_Lib.ObjectPool.RunTime; 
+using PSB_Lib.Dependencies;      
+using Random = UnityEngine.Random;
 
 namespace Code.Scripts.Items.Core
 {
     public class ManaDropMap : MonoBehaviour
     {
         public Transform player;
-        public GameObject itemPrefab;
         public int maxItems = 30;
         public float spawnRadius = 10f;
         public float minDistanceFromPlayer = 1f;
@@ -14,10 +16,16 @@ namespace Code.Scripts.Items.Core
         public float minSpawnDelay = 0.5f;
         public float maxSpawnDelay = 2f;
 
+        [SerializeField] private PoolItemSO dropItemPrefab; 
+        [Inject] private PoolManagerMono _poolManager;
+
         private int spawnedCount = 0;
 
         private void Start()
         {
+            if (_poolManager == null)
+                _poolManager = FindObjectOfType<PoolManagerMono>();
+            
             StartCoroutine(SpawnRoutine());
         }
 
@@ -28,13 +36,12 @@ namespace Code.Scripts.Items.Core
                 float delay = Random.Range(minSpawnDelay, maxSpawnDelay);
                 yield return new WaitForSeconds(delay);
 
-                
-                // 여기서 생성 부분 처리 (랜덤 위치 구하고 Instantiate 호출 등)
                 Vector2 pos = (Vector2)player.position + Random.insideUnitCircle * spawnRadius;
                 if (Vector2.Distance(pos, player.position) < minDistanceFromPlayer) continue;
+                
+                var dropItem = _poolManager.Pop<PickUpItem>(dropItemPrefab);
+                dropItem.transform.position = pos;
 
-                //Instantiate(itemPrefab, pos, Quaternion.identity);
-                Debug.Log(pos);
                 spawnedCount++;
             }
         }
